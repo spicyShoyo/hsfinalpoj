96994845850b7e6e28b602a7c376063d1ba6873c
spicyShoyo <lzhengchao18@outlook.com>
2016-11-22 14:00:26 -0600

    s

--- cesna.py
+++ cesna.py
@@ -15,11 +15,14 @@ class Cesana:
         self.x_mat = self.get_x_mat() #exclude ego, numpy array, node index starting from 0
         self.f_mat = np.zeros((len(self.network.node_feat)-1, CIRCLE_NUM)) #exclude ego, numpy array
         self.f_mat[:, -1] = 1   #add one for not having zero
-        self.w_mat = np.random.rand((len(self.network.featname_list) * CIRCLE_NUM)).reshape((len(self.network.featname_list), CIRCLE_NUM))
+        # self.w_mat = np.random.rand((len(self.network.featname_list) * CIRCLE_NUM)).reshape((len(self.network.featname_list), CIRCLE_NUM))
+        self.w_mat = np.zeros((len(self.network.featname_list), CIRCLE_NUM))
         self.k_num = (len(self.network.featname_list))
+        self.w_mat[:, -1] += 1 / self.k_num
         self.u_num = len(self.idx_dic)
         self.new_f_mat = np.zeros((len(self.network.node_feat)-1, CIRCLE_NUM))
         self.new_w_mat = np.zeros((len(self.network.featname_list), CIRCLE_NUM))
+        self.delta = (-np.log(1 - 1 / self.u_num)) ** 0.5
 
     def get_x_mat(self):
         feat_dic = {}
@@ -45,9 +48,7 @@ class Cesana:
         =========================
         '''
     def q_uk(self, u, k):
-        s = 0
-        for c in range(CIRCLE_NUM):
-            s += self.w_mat[k][c] * self.f_mat[u][c]
+        s = self.w_mat[k] @ self.f_mat[u]   #should .T in theory, but numpy ignores that
         return 1 / (1 + np.exp(-s))
 
     def d_lg_fu(self, u, c):
@@ -115,14 +116,16 @@ class Cesana:
         self.get_circle()
 
     def get_circle(self):
+        print(self.f_mat)
+        print(self.w_mat)
         for c in range(CIRCLE_NUM):
             print("Cirlce", c, end="")
             for u in range(self.u_num):
-                if self.f_mat[u][c] == 1:
+                if self.f_mat[u][c] > self.delta:
                     print(", ", self.idx_dic_back[u],  end="")
             print()
 
 
 a = Cesana(0)
-a.get_circle()
-a.update()
+for i in range(2):
+    a.update()
